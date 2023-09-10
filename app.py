416c9cc341c4e2953a2344c735aaceb887ee7542
Vedant Gune <vedantgune41@gmail.com>
2023-09-11 01:39:09 +0530

    refactoring

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
-from flask import request, redirect, flash, url_for
 
+from flask import Flask, render_template, request, redirect, flash, url_for
 import pickle
 
-
 app = Flask(__name__)
+app.secret_key = "mywebsite2023"
+
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
 
 
 @app.route('/')
@@ -17,36 +21,6 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/health_test', methods=["GET", "POST"])
-def health_test():
-    datapts = []
-    try:
-        if request.method == "POST":
-            req = request.form
-            for x in req.values():
-                datapts.append(x)
-
-            datapts = [int(x) for x in datapts[1:]]
-            model_loaded = pickle.load(open('./MHSModel.pkl', 'rb'))
-            prediction = model_loaded.predict([datapts])
-
-            if (prediction[0] == 0):
-                flash('Your test results are negetive!', 'success')
-                return redirect(url_for('health_test'))
-
-            else:
-                flash(
-                    "Your test results are Positive, Please see a good Psychiatrist!", 'error')
-                return redirect(url_for('health_test'))
-
-        else:
-            return render_template('health_test.html')
-
-    except Exception as e:
-        pass
-        # flash("An error occurred!", 'error')
-
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
@@ -57,6 +31,29 @@ def support():
     return render_template('support.html')
 
 
+@app.route('/health_test', methods=["GET", "POST"])
+def health_test():
+    try:
+        if request.method == "POST":
+            datapts = [int(x) for x in request.form.values() if x.isdigit()]
+            if not datapts:
+                flash('Invalid input. Please enter numeric values.', 'error')
+            else:
+                model_loaded = pickle.load(open('./MHSModel.pkl', 'rb'))
+                prediction = model_loaded.predict([datapts])
+
+                if prediction[0] == 0:
+                    flash('Your test results are negative!', 'success')
+                else:
+                    flash(
+                        'Your test results are positive. Please see a good Psychiatrist!', 'error')
+            return redirect(url_for('health_test'))
+        else:
+            return render_template('health_test.html')
+    except Exception as e:
+        flash("An error occurred: " + str(e), 'error')
+
+
 if __name__ == "__main__":
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
@@ -64,6 +61,4 @@ if __name__ == "__main__":
     except ValueError:
         PORT = 5555
 
-    app.secret_key = "mywebsite2023"
-    app.run(HOST, port=PORT)
-    app.run(debug=False, port=8000)
+    app.run(HOST, port=PORT, debug=True)  # Use debug=True for development
