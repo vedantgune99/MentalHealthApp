661b6bd02cfdef0fe1f5f160f430f92655193a70
Vedant Gune <vedantgune41@gmail.com>
2023-09-11 18:59:36 +0530

    email feature, ml model improved & ready to deploy

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_mail import Mail, Message
-from config.py import emailID, password, SECRET_KEY
+from config import emailID, password, SECRET_KEY
 import pickle
 
+
+app = Flask(__name__)
+app.secret_key = SECRET_KEY
 
 # configuration of mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -13,10 +16,6 @@ app.config['MAIL_PASSWORD'] = password
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
-
-
-app = Flask(__name__)
-app.secret_key = SECRET_KEY
 
 
 @app.route('/')
@@ -29,13 +28,14 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/contact', method=['POST', 'GET'])
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
         req = request.form
-        user_name = request.form.values()[0]
-        user_email = request.form.values()[1]
-        user_message = request.form.values()[2]
+        collected = [x for x in req.values()]
+        user_name = collected[0]
+        user_email = collected[1]
+        user_message = collected[2]
 
         msg = Message(
             'Support Required',
@@ -43,10 +43,10 @@ def contact():
             recipients=[user_email]
         )
         msg.body = f'''
-            Name : {user_name}\n
-            Email : {user_email}\n\n
-            Message : {user_message}\n
-            Dear {user_name},\n\t
+            Name : {user_name}
+            Email : {user_email}
+            Message : {user_message}
+            Dear {user_name},
                     Welcome, Your issue will be resolved as soon as possible, we will contact you back soon!
                     \nThank You!
             '''
@@ -90,4 +90,4 @@ def health_test():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
